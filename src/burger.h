6f4616f19b1987d136fc60d46780a56afa1d0599
Your Name <you@example.com>
2024-04-29 14:13:26 -0700

    compare the two stacks

--- src/burger.h
+++ src/burger.h
@@ -13,8 +13,10 @@ using namespace std;
 
 class Burger {
 
-    stack<int> items;
+    stack<int> items;//burger items
+    stack<int> user_burger;
     map<int, string> ingredients;
+
 
     void initialize_ingredients();
 
@@ -27,6 +29,8 @@ public:
     void display();
 
     friend bool operator==(const Burger& comp1, const Burger& comp2);
+    
+    bool order_Issame() const;
 
 };
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,19 @@
 #include <iostream>
 #include <stdlib.h>
 #include "burger.h"
-
+#include "burger.cpp" //leave this here or my compiler breaks
 using namespace std;
 
 //temp calls
 void playGame() {
 
     //system("cls"); // Clear terminal for every new game. Buggy in vscode terminal. Works when you run the .exe
-
     cout << "Playing the game\n"<< endl;
-
     // Functionality for playing the game
+    
     srand(time(nullptr));
-
-    int random = rand() % 10 + 1;
-    Burger order(random);
+    int random_burgersize = rand() % 10 + 1;
+    Burger order(random_burgersize);
 
     order.display();
 
@@ -36,7 +34,7 @@ int main() {
         // Display menu options
         cout << "\n=== Game Menu ===\n";
         cout << "1. Play Game\n";
-        cout << "2. New Game\n";
+        cout << "2. New Game\n";//should adjust the game idk (not sure if needed)
         cout << "3. Exit\n";
         cout << "Enter your choice: ";
         cin >> choice;
