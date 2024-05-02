9371a1830827e83b2015ca3a843e45f4c124d6be
Bjorneee <benicimo4@gmail.com>
2024-05-02 12:31:15 -0700

    Deleted unused functions from burger

--- src/burger.cpp
+++ src/burger.cpp
@@ -12,16 +12,6 @@ Burger::Burger(int size) {
     items.push(1); // All burgers end with a top bun
 }
 
-Burger::Burger(stack<int>& burgerStack) {
-
-    items.push(0);
-    while(!burgerStack.empty()){
-        items.push(burgerStack.top());
-    }
-    items.push(1);
-
-}
-
 void Burger::display(map<int, string>& ingredient_map){
 
     ifstream ifs;
@@ -64,25 +54,3 @@ bool operator==(const Burger& comp1, const Burger& comp2) {
 
     return comp1.items == comp2.items;
 }
-
-/*
-//stack comapre idk if you want to user this
-bool Burger:: order_Issame() const {
-        stack<int> items_copy = items;
-        stack<int> user_burger_copy = user_burger;
-
-        // Compare the sizes of the stacks
-        if (items_copy.size() != user_burger_copy.size()) {
-            return false; // Stacks are of different sizes
-        }
-        while (!items_copy.empty()) {
-            if (items_copy.top() != user_burger_copy.top()) {
-                return false; // Elements at the top of both stacks are different
-            }
-            items_copy.pop();
-            user_burger_copy.pop();
-        }
-
-        return true;
-};
-*/
--- src/burger.h
+++ src/burger.h
@@ -15,17 +15,11 @@ using namespace std;
 class Burger {
 
     stack<int> items;//burger items
-    stack<int> user_burger;
     map<int, string> ingredients;
-
-
-    void initialize_ingredients();
 
 public:
 
     Burger(int size);
-
-    Burger(stack<int>& burgerStack);
 
     void display(map<int, string>& ingredients);
 
@@ -33,8 +27,6 @@ public:
     
     friend bool operator==(const Burger& comp1, const Burger& comp2);
     
-    bool order_Issame() const;
-
 };
 
 #endif //BURGER_H
