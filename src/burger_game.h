ff650a6ed6bc904f56a1abd34fa8ee0a39b76dd8
7HE-LUCKY-FISH <bryanlin1968@gmail.com>
2024-05-02 17:53:32 -0700

    clean up

--- src/burger_game.h
+++ src/burger_game.h
@@ -8,21 +8,19 @@
 #include <sstream>
 #include <stdlib.h>
 using namespace std;
-class game_state{
-    private:
+class game_state
+{
+private:
     unordered_map<string, int> scores;
     int user_score;
     string user_name;
-    public:
-    
+
+public:
     void write_score(int score);
     int find_highest_score();
-    void saveScores(const unordered_map<string, int>& scores, const string& filename);
-    int getScore( const string& playerName,const string& filename);
+    void saveScores(const unordered_map<string, int> &scores, const string &filename);
 
-
-
+    int getScore(const string &playerName, const string &filename);
 };
-
 
 #endif
